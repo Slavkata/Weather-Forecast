9232edb47c8a9a1c98b61bf3ae3d0b2b08edbf77
Slavkata <slavkirilov00@gmail.com>
2017-11-02 16:41:10 +0200

    Adding a way to use GridSearchCV

--- Temperature_only/temperature_only.py
+++ Temperature_only/temperature_only.py
@@ -1,8 +1,6 @@
 import pandas as p
 import sys
-import math
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Ridge
 from sklearn.pipeline import make_pipeline
@@ -19,14 +17,13 @@ def getY(data):
     data = data.rename(columns = {'T' : 'Tm'})
     return data [['Tm']].as_matrix()
 
-X_path = './trainData.csv'
-y = getY(getData('./trainData.csv'))
+X_path = './tData.csv'
+y = getY(getData('./tData.csv'))
 X = getData(X_path)
 
-pl = make_pipeline(Ridge(alpha = 2, fit_intercept = True))
+pl = make_pipeline(XTransformer(), Ridge(alpha = 2, fit_intercept = True))
 
-X = XTransformer().transform(getData(X_path))
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 42)
 
 pl.fit(X_train, y_train)
 
